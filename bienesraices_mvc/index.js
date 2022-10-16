e1e5ee5b094bd10aa4f1203c485cc79825cf7c0e
unknown <fmendoza.ospina7@outlook.com>
2022-10-16 08:48:27 -0500

    Node

--- bienesraices_mvc/index.js
+++ bienesraices_mvc/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import db from './config/db.js';
 
 // Crear la app.
 const app = express();
+
+// Conexión a la base de datos.
+try {
+    await db.authenticate();
+    console.log("Conexión correcta a la base de datos");
+    
+} catch(error){
+    console.log(error);
+}
 
 // Habilitar Pug.
 app.set('view engine','pug');
