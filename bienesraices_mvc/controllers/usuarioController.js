400f1b23d8b18189706d6c3f09cacd5deb0ca1dc
unknown <fmendoza.ospina7@outlook.com>
2022-10-21 04:59:37 -0500

    Node

--- bienesraices_mvc/controllers/usuarioController.js
+++ bienesraices_mvc/controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from "../models/Usuario.js";
 
 const formularioLogin = (req, res) => {
@@ -13,6 +14,17 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    // Validación.
+    await check('nombre').notEmpty().withMessage('El nombre no puede ir vacío').run(req);
+    await check('email').isEmail().withMessage('El email no es válido').run(req);
+    await check('password').isLength({ min: 6 }).withMessage('El password debe ser de al menos 6 caracteres').run(req);
+    await check('repetir_password').equals('password').withMessage('Los passwords no coinciden').run(req);
+    let resultado = validationResult(req);
+
+    // Verificar que no haya errores.
+    
+    res.json(resultado.array());
+
     const usuario = await Usuario.create(req.body);
     res.json(usuario);
 }
