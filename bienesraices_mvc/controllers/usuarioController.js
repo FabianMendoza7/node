d2af2dd95930678fbf5e6ec57469548d504a18e0
unknown <fmendoza.ospina7@outlook.com>
2022-11-07 10:17:07 -0500

    Node

--- bienesraices_mvc/controllers/usuarioController.js
+++ bienesraices_mvc/controllers/usuarioController.js
@@ -1,7 +1,8 @@
 import { check, validationResult } from 'express-validator'
+import bcrypt from 'bcrypt'
 import Usuario from "../models/Usuario.js";
 import { generarId } from '../helpers/tokens.js';
-import { emailRegistro } from '../helpers/emails.js';
+import { emailRegistro, emailOlvidePassword } from '../helpers/emails.js';
 
 const formularioLogin = (req, res) => {
     res.render('auth/login',{
@@ -140,7 +141,73 @@ const resetPassword = async (req, res) => {
     }
 
     // Generar el token y enviar el email.
+    usuario.token = generarId();
+    await usuario.save();
+
+    // Enviar un email.
+    emailOlvidePassword({
+        email: usuario.email,
+        nombre: usuario.nombre,
+        token: usuario.token
+    });
+
+    // Mostrar mensaje de confirmación.
+    res.render('templates/mensaje', {
+        pagina: 'Reestablece tu Password',
+        mensaje: 'Hemos Enviado un Email con las instrucciones'
+    })
+}
+
+const comprobarToken = async (req, res) => {
+    const { token } = req.params;
+    const usuario = await Usuario.findOne({where: {token}})
     
+    if(!usuario){
+        return res.render('auth/confirmar-cuenta', {
+            pagina: 'Reestablece tu password',
+            mensaje: 'Hubo un error al validar tu información, intenta de nuevo',
+            error: true
+        })
+    }
+
+    // Mostrar formulario para modificar el password.
+    res.render('auth/reset-password', {
+        pagina: 'Reestablece tu Password',
+        csrfToken: req.csrfToken(),
+    })
+}
+
+const nuevoPassword = async (req, res) => {
+    // Validar nuevo passwod.
+    await check('password').isLength({ min: 6 }).withMessage('El password debe ser de al menos 6 caracteres').run(req);
+    let resultado = validationResult(req);
+
+    // Verificar que no haya errores.
+    if(!resultado.isEmpty()){
+        // Errores.
+        return res.render('auth/reset-password',{
+            pagina: 'Reestablece tu Password',
+            csrfToken: req.csrfToken(),
+            errores: resultado.array()
+        });
+    }
+
+    const { token } = req.params;
+    const { password } = req.body;
+
+    // Identificar el usuario.
+    const usuario = await Usuario.findOne({where: {token}})
+    
+    // Hashear el nuevo password.
+    const salt = await bcrypt.genSalt(10)
+    usuario.password = await bcrypt.hash(password, salt)
+    usuario.token = null; 
+    await usuario.save();
+
+    res.render('auth/confirmar-cuenta', {
+        pagina: 'Password Reestablecido',
+        mensaje: 'El Password se guardó correctamente'
+    })
 }
 
 export {
@@ -149,5 +216,7 @@ export {
     registrar,
     confirmar,
     formularioOlvidePassword,
-    resetPassword
+    resetPassword,
+    comprobarToken,
+    nuevoPassword
 }
--- bienesraices_mvc/helpers/emails.js
+++ bienesraices_mvc/helpers/emails.js
@@ -31,6 +31,38 @@ const emailRegistro = async (datos) => {
     })
 }
 
+const emailOlvidePassword = async (datos) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const { nombre, email, token } = datos;
+
+    // Enviar el email mediante nodemailer con las credenciales de mailtrap.
+    await transport.sendMail({
+        from: 'BienesRaices.com',
+        to: email,
+        subject: 'Reestablece tu password en BienesRaices.com',
+        text: 'Reestablece tu password BienesRaices.com',
+        html: `
+            <p>Hola ${nombre}, has solicitado reestablecer tu password en BienesRaices.com</p>
+
+            <p>
+                Sigue el enlace para generar una nuevo password:
+                <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/olvide-password/${token}">Reestablecer Password</a>
+            </p>
+
+            <p>Si no solicitaste el cambio de password, puedes ignorar el mensaje</p>
+        `
+    })
+}
+
 export {
-    emailRegistro
+    emailRegistro,
+    emailOlvidePassword
 }
