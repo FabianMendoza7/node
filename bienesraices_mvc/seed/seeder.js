457a05d4ae5a5eb06e893c20b4fb81c8902332bf
unknown <fmendoza.ospina7@outlook.com>
2022-12-09 00:03:12 -0500

    Node

---
+++ bienesraices_mvc/seed/seeder.js
@@ -0,0 +1,27 @@
+import { exit } from 'node:process'
+import categorias from './categorias.js'
+import Categoria from '../models/Categoria.js'
+import db from '../config/db.js'
+
+const importarDatos = async () => {
+    try {
+        // Autenticar.
+        await db.authenticate()
+
+        // Generar las columnas.
+        await db.sync()
+
+        // Insertamos los datos.
+        await Categoria.bulkCreate(categorias)
+        console.log('Datos importados correctamente!')
+        exit()
+
+    } catch (error) {
+        console.log(error)
+        exit(1)
+    }
+}
+
+if(process.argv[2] === "-i"){
+    importarDatos()
+}
