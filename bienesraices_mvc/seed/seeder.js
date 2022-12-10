bd6405211d6044e5b88b775b2714bff36acfed4c
unknown <fmendoza.ospina7@outlook.com>
2022-12-09 22:22:32 -0500

    Node

--- bienesraices_mvc/seed/seeder.js
+++ bienesraices_mvc/seed/seeder.js
@@ -1,6 +1,8 @@
 import { exit } from 'node:process'
 import categorias from './categorias.js'
+import precios from './precios.js'
 import Categoria from '../models/Categoria.js'
+import Precio from '../models/Precio.js'
 import db from '../config/db.js'
 
 const importarDatos = async () => {
@@ -12,7 +14,10 @@ const importarDatos = async () => {
         await db.sync()
 
         // Insertamos los datos.
-        await Categoria.bulkCreate(categorias)
+        await Promise.all([
+            Categoria.bulkCreate(categorias),
+            Precio.bulkCreate(precios)])
+
         console.log('Datos importados correctamente!')
         exit()
 
@@ -22,6 +27,26 @@ const importarDatos = async () => {
     }
 }
 
+const eliminarDatos = async () => {
+    try{
+        await Promise.all([
+            Categoria.destroy({where: {}, truncate: true}),
+            Precio.destroy({where: {}, truncate: true})])
+
+        // Otra forma:
+        //await db.sync({force: true})
+        console.log("Datos eliminados correctamente")
+        exit()
+    } catch (error) {
+        console.log(error)
+        exit(1)
+    }
+}
+
 if(process.argv[2] === "-i"){
     importarDatos()
+}
+
+if(process.argv[2] === "-e"){
+    eliminarDatos()
 }
