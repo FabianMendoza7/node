e5a6a077ac44405f99bdc8723931f8c953eb9f30
unknown <fmendoza.ospina7@outlook.com>
2022-12-23 12:38:30 -0500

    Node

--- bienesraices_mvc/controllers/propiedadController.js
+++ bienesraices_mvc/controllers/propiedadController.js
@@ -52,6 +52,7 @@ const guardar = async (req, res) => {
 
     // Crear un registro.
     const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body
+    const { id: usuarioId } = req.usuario
 
     try {
         const propiedadGuardada = await Propiedad.create({
@@ -64,8 +65,13 @@ const guardar = async (req, res) => {
             lat, 
             lng,
             precioId,
-            categoriaId          
+            categoriaId,
+            usuarioId,
+            imagen: ''
         })
+
+        const { id } = propiedadGuardada
+        res.redirect(`/propiedades/agregar-imagen/${id}`)
 
     } catch(error) {
         console.log(error)
