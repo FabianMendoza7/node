1e96b08e15b6f148b4eb2868e9d8170e20aaa580
unknown <fmendoza.ospina7@outlook.com>
2023-02-09 13:22:05 -0500

    chatbot

--- chatbot-messenger/config.js
+++ chatbot-messenger/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAhWhLFj4DUBABNPVZAZAoH5ctyzqNKKRyZCc743PjdXKoxqVZCfnQmM93oWSS8Igk5gcuwnv9qCkSESMwRVTaN9M9rqZCZBQhAiDnMZAZC0U4vFh0LC7uCzIqRcMIYg9Y3erqTRXpZC9OCq1hdj5zxpvZB9TuZC4MxGFIutZBsZAHIDnqek0hGUkqc3Q',
+    FB_VERIFY_TOKEN: 'my_voice_and_my_password-ud-co-bot1',
     FB_APP_SECRET: '',
-    SERVER_URL: '',
+    SERVER_URL: 'http://mindup.com/',
     GOOGLE_PROJECT_ID: '',
     DF_LANGUAGE_CODE: '',
     GOOGLE_CLIENT_EMAIL: '',
