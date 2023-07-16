2177a8f376f794c6a2e3ac540c9161b7f1f30cc8
unknown <fmendoza.ospina7@outlook.com>
2023-07-16 07:39:16 -0500

    App pages

--- bienesraices_mvc/index.js
+++ bienesraices_mvc/index.js
@@ -3,6 +3,7 @@ import csrf from 'csurf';
 import cookieParser from 'cookie-parser';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import propiedadRoutes from './routes/propiedadRoutes.js';
+import appRoutes from './routes/appRoutes.js';
 import db from './config/db.js';
 
 // Crear la app.
@@ -35,6 +36,7 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 // Routing.
+app.use('/', appRoutes);
 app.use('/auth', usuarioRoutes);
 app.use('/', propiedadRoutes);
 
