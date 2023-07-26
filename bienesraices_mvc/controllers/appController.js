6f7fb32c5f05388b5011817d92326aff8330faf5
unknown <fmendoza.ospina7@outlook.com>
2023-07-26 08:46:33 -0500

    NodeJS:Filtering search results

--- bienesraices_mvc/controllers/appController.js
+++ bienesraices_mvc/controllers/appController.js
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize'
 import {Precio, Categoria, Propiedad} from '../models/index.js'
 
 const inicio = async (req, res) => {
@@ -43,7 +44,8 @@ const inicio = async (req, res) => {
         categorias,
         precios,
         casas,
-        departamentos
+        departamentos,
+        csrfToken: req.csrfToken()
     })
 }
 
@@ -69,18 +71,43 @@ const categoria = async (req, res) => {
 
     res.render('categoria', {
         pagina: `${categoria.nombre}s en Venta`,
-        propiedades
+        propiedades,
+        csrfToken: req.csrfToken()
     })
 }
 
 const noEncontrado = (req, res) => {
     res.render('404', {
-        pagina: 'No Encontrada'
+        pagina: 'No Encontrada',
+        csrfToken: req.csrfToken()
     })
 }
 
-const buscador = (req, res) => {
-    
+const buscador = async (req, res) => {
+    const { termino } = req.body
+
+    // Validar que termino no esté vacío.
+    if(!termino.trim()){
+        return res.redirect('back')
+    }
+
+    // Consultar las propiedades.
+    const propiedades = await Propiedad.findAll({
+        where: {
+            titulo: {
+                [Sequelize.Op.like] : '%' + termino + '%'
+            }
+        },
+        include: [
+            { model: Precio, as: 'precio'}
+        ]
+    })
+
+    res.render('busqueda', {
+        pagina: 'Resultados de la Búsqueda',
+        propiedades,
+        csrfToken: req.csrfToken()
+    })
 }
 
 export {
