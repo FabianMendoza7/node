46304221e87af690e76cbbf9b8b3ceee9e36f160
unknown <fmendoza.ospina7@outlook.com>
2023-07-29 07:37:02 -0500

    NodeJS: Adding contact form

--- bienesraices_mvc/controllers/propiedadController.js
+++ bienesraices_mvc/controllers/propiedadController.js
@@ -1,6 +1,7 @@
 import { unlink } from 'node:fs/promises'
 import { validationResult } from 'express-validator'
 import { Precio, Categoria, Propiedad } from '../models/index.js'
+import { esVendedor } from '../helpers/index.js'
 
 const admin = async (req, res) => {
     // Leer QueryString.
@@ -310,11 +311,14 @@ const mostrarPropiedad = async (req, res) => {
         return res.redirect('/404')
     }
 
+    console.log()
+
     res.render('propiedades/mostrar', {
         propiedad,
         pagina: propiedad.titulo,
         csrfToken: req.csrfToken(),
-        usuario: req.usuario
+        usuario: req.usuario,
+        esVendedor: esVendedor(req.usuario?.id, propiedad.usuarioId)
     })
 }
 
