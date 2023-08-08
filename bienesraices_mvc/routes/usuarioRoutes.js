11720a0527c59c0c50d9e1c8e0f3ce4b5791ba10
unknown <fmendoza.ospina7@outlook.com>
2023-08-08 05:54:33 -0500

    NodeJS: closing session

--- bienesraices_mvc/routes/usuarioRoutes.js
+++ bienesraices_mvc/routes/usuarioRoutes.js
@@ -8,12 +8,17 @@ import {
     formularioOlvidePassword, 
     resetPassword, 
     comprobarToken, 
-    nuevoPassword } from '../controllers/usuarioController.js';
+    nuevoPassword,
+    cerrarSesion } from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 router.get('/login', formularioLogin);
 router.post('/login', autenticar);
+
+// Cerrar sesión.
+router.post('/cerrar-sesion', cerrarSesion);
+
 router.get('/registro', formularioRegistro);
 router.post('/registro', registrar);
 router.get('/confirmar/:token', confirmar);
