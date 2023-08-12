23df0c94b17ace73173c1fa5ecc5dcba7d76b5fd
unknown <fmendoza.ospina7@outlook.com>
2023-08-12 14:42:08 -0500

    NodeJS: Definning the model of vacantes

--- devjobs/index.js
+++ devjobs/index.js
@@ -1,7 +1,12 @@
+const mongoose = require("mongoose");
+require("./config/db");
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 require('dotenv').config({path: 'variables.env'});
 
 const app = express();
@@ -17,6 +22,16 @@ app.set('view engine', 'handlebars');
 
 // Static files.
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser);
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 app.use('/', router());
 
