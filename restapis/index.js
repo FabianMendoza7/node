cf0ccb70d65e37926100ace4635d684a1323d037
unknown <fmendoza.ospina7@outlook.com>
2023-08-18 16:38:41 -0500

    Node: RestApis with mongodb and mongoose

--- restapis/index.js
+++ restapis/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+// Conectar mongo.
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/restapis', {
+//     useNewUrlParser: true
+// });
 
 // Crear el servidor.
 const app = express();
+
+// Rutas de las apps.
+app.use('/', routes());
 
 // Puerto.
 app.listen(5000);
