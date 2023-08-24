a9348427c1731576ce336c1d89986255d4559d2d
unknown <fmendoza.ospina7@outlook.com>
2023-08-24 08:40:10 -0500

    Node: Creating Products model

--- restapis/routes/index.js
+++ restapis/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const clienteController = require('../controllers/clienteController');
+const productosController = require('../controllers/productosController');
 const router = express.Router();
 
 module.exports = function() {
+
+    /** CLIENTES **/
     
     // Agrega nuevos clientes via POST.
     router.post('/clientes', clienteController.nuevoCliente);
@@ -18,6 +21,27 @@ module.exports = function() {
 
     // Eliminar un cliente.
     router.delete('/clientes/:idCliente', clienteController.eliminarCliente);    
+
+    /** PRODUCTOS **/
+
+    // Agrega nuevo producto.
+    router.post('/productos', 
+        productosController.subirArchivo,
+        productosController.nuevoProducto);
+
+    // Mostrar productos.
+    router.get('/productos', productosController.mostrarProductos);
+
+    // Mostrar producto por id.
+    router.get('/productos/:idProducto', productosController.mostrarProducto);
+
+    // Actualizar un producto.
+    router.put('/productos/:idProducto',
+        productosController.subirArchivo,
+        productosController.actualizarProducto);
+
+    // Eliminar un producto.
+    router.delete('/productos/:idProducto', productosController.eliminarProducto);
 
     return router;
 }
